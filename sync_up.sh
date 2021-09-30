244cb8e8ef6aa27d533a0c906536233359dcb12e
bloatphobic <mail@bloatphobic.com>
2021-09-30 05:25:43 +0200

    Swap left-alt with left-crtl

--- sync_up.sh
+++ sync_up.sh
@@ -6,6 +6,8 @@ cp ~/.zshrc ~/dotfiles/zsh/.zshrc
 
 cp ~/.xinitrc ~/dotfiles/xorg/.xinitrc
 
+cp ~/.Xmodmap ~/dotfiles/xorg/.Xmodmap
+
 cp ~/.config/i3/config ~/dotfiles/i3/config
 
 cp ~/.config/i3status/config ~/dotfiles/i3status/config
